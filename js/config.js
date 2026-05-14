8e2a7d95903f382c8e4b5ca9b7eadfe93b366825
Autumn Jing Green <67246658+autumngreenbean@users.noreply.github.com>
2026-05-14 14:03:39 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -12,7 +12,7 @@
 
 const CONFIG = {
   // Apps Script URL (only used for contact form submissions now)
-  GOOGLE_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyXC6J33xRjrRXlLT3Zukpo2r02kv0qiMrYpKkY6v8N51-3zxgXif-95cdwLtNvLetm/exec',
+  GOOGLE_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyR891IPYOqbJ5hvfGbWRtRAHVJDFZnASPoUb8lxxyUs_8-jf_QX5p3DAtedpS_IyQ-/exec',
   
   // Cache duration in milliseconds (5 minutes)
   // Website now loads from fast static JSON, updated automatically by Google Sheets!
